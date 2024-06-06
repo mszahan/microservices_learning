35ccde2cd1aaa860b47a9682976f6129d175d03b
mszahan <mszahan1@gmail.com>
2024-06-06 23:32:29 +0600

    added form and form view for the subsciption page

---
+++ dj_ms/subscription/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from subscription.views import SubscriptionFormView, SuccessView
+
+
+app_name = 'subscription'
+
+
+urlpatterns = [
+    path('', SubscriptionFormView.as_view(), name='subscription'),
+    path('success/', SuccessView.as_view(), name='success'),
+]
--- dj_ms/subscription/views.py
+++ dj_ms/subscription/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from typing import Any
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
+from subscription.forms import SubscriptionForm
+from subscription.models import Address
 
-# Create your views here.
+
+class SubscriptionFormView(FormView):
+    template_name = 'subscription/subscription.html'
+    form_class = SubscriptionForm
+    success_url = '/success/'
+
+    def form_valid(self, form):
+        address = Address(name=form.cleaned_data['name'],
+                          address=form.cleaned_data['address'],
+                          postalcode=form.cleaned_data['postalcode'],
+                          city=form.cleaned_data['city'],
+                          country=form.cleaned_data['country'],
+                          email=form.cleaned_data['email'])
+        address.save()
+        self.request.session['name'] = form.cleaned_data['name']
+        return super().form_valid(form)
+
+
+
+class SuccessView(TemplateView):
+    template_name = 'subscription/success.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = self.request.session.get('name', 'Subscriber')
+        return context
