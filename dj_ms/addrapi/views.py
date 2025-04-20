4845124d9a3019b1bb959c4d11e5a0bcf160f07d
mszahan <mszahan1@gmail.com>
2025-04-20 17:11:55 +0600

    added the post method to the address api list

--- dj_ms/addrapi/views.py
+++ dj_ms/addrapi/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from subscription.models import Address
 from .serializers import AddressSerializer
 
@@ -10,3 +11,11 @@ class AddressList(APIView):
         addresses = Address.objects.all()
         serializer = AddressSerializer(addresses, many=True)
         return Response(serializer.data)
+    
+    def post(self, request, format=None):
+        serializer = AddressSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
--- dj_ms/config/settings.py
+++ dj_ms/config/settings.py
@@ -54,7 +54,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #third party
     'rest_framework',
+
+    #local
     'subscription',
     'addrapi',
 ]
