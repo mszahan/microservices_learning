c177f11fa1a93605ee739b3d9f3d884131e6e081
mszahan <mszahan1@gmail.com>
2025-04-20 18:06:50 +0600

    created CRUD endpoints for address list and detail view with mixin and generic

--- dj_ms/addrapi/urls.py
+++ dj_ms/addrapi/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import AddressList
+from .views import AddressList, AddressDetail
 
 ver = 'v1'
 
 urlpatterns = [
-    path(f'{ver}/addresses', AddressList.as_view())
+    path(f'{ver}/addresses/', AddressList.as_view()),
+    path(f'{ver}/addresses/<int:pk>/', AddressDetail.as_view()),
 ]
--- dj_ms/addrapi/views.py
+++ dj_ms/addrapi/views.py
@@ -1,21 +1,57 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+from rest_framework import mixins, generics
 from subscription.models import Address
 from .serializers import AddressSerializer
 
 
 
-class AddressList(APIView):
-    def get(self, request, format=None):
-        addresses = Address.objects.all()
-        serializer = AddressSerializer(addresses, many=True)
-        return Response(serializer.data)
+
+class AddressList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
     
-    def post(self, request, format=None):
-        serializer = AddressSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class AddressDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    
+    def patch(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+
+# class AddressList(APIView):
+#     def get(self, request, format=None):
+#         addresses = Address.objects.all()
+#         serializer = AddressSerializer(addresses, many=True)
+#         return Response(serializer.data)
+    
+#     def post(self, request, format=None):
+#         serializer = AddressSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
